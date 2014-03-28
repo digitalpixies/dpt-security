72892f64d96dc6518ebb32a4d948cb1ed495b5ca
Robert Huie <rhuie@digitalpixies.com>
2014-03-27 20:59:04 -0700

    version 1.0.0

---
+++ dpt-security.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Wordpress Security by DigitialPixies
+Plugin URI: http://wordpress.digitalpixies.com/dpt-security
+Description: Security Improvements to standard wordpress installs
+Version: 3.8.1
+Author: Robert Huie
+Author URI: http://DigitalPixies.com
+License: GPLv2
+*/
+
+if(!class_exists("dpt_security_php")) {
+	class dpt_security_php {
+		public static $data = null;
+		public static function RegisterHooks() {
+            wp_enqueue_script('jquery');
+			add_action('login_head', 'dpt_security_php::ReformatFormJS');
+			add_filter('wp_authenticate_user', 'dpt_security_php::AuthenticateUser');
+		}
+		public static function Initialize() {
+			session_start();
+			if(!isset($_SESSION[__CLASS__]))
+				$_SESSION[__CLASS__]=array(
+					"modulator_name"=>md5(uniqid())
+					, "modulation_value"=>uniqid());
+			dpt_security_php::$data=&$_SESSION[__CLASS__];
+		}
+		public static function ReformatFormJS() {
+			dpt_security_php::Initialize();
+			$data = &dpt_security_php::$data;
+			print <<<EOF
+<script type="text/javascript">
+	(function($) {
+		$(document).ready(function() {
+			$("#loginform").append('<input type="hidden" name="{$data["modulator_name"]}" value="{$data["modulation_value"]}" />');
+		});
+	})(jQuery);
+</script>
+EOF;
+		}
+		public static function AuthenticateUser($username, $password) {
+			dpt_security_php::Initialize();
+			if($_REQUEST[dpt_security_php::$data["modulator_name"]]==dpt_security_php::$data["modulation_value"])
+				return $username;
+			$error = new WP_Error();
+			$error->add('no_javascript', "Javascript needs to be enabled.");
+			return $error;
+		}
+	}
+}
+
+dpt_security_php::RegisterHooks();
+
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Plugin Name ===
+Contributors: digitalpixies
+Donate link: http://digitalpixies.com/
+Tags: login, security, authentication
+Requires at least: 3.0.1
+Tested up to: 3.8.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Security Improvements to standard wordpress installs.
+
+== Description ==
+
+Enforce javascript during login as a means to minimize brute-force attackers from gaining your account passwords. More enhancements will be added in months to come.
+
+* Enforce Javascript at login
+
+== Installation ==
+
+1. Upload `dpt-security` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
